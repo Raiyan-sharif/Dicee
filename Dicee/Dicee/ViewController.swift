93d32293bd967b419e91ec5f729abb58a6e72676
Raiyan <raiyan.sharif.1234@gmail.com>
2019-09-23 00:35:16 +0600

    gitignore file added

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by raiyan sharif on 23/9/19.
+//  Copyright © 2019 raiyan sharif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
